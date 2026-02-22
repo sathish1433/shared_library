498f5a808d6a29774e0daf178f27789a7ea277e4
Sathish <76042316+sathish1433@users.noreply.github.com>
2026-02-22 17:30:34 +0530

    Update pyapp.groovy

--- vars/pyapp.groovy
+++ vars/pyapp.groovy
@@ -3,7 +3,7 @@ def call(Map config=[:]){
         git changelog: false, poll: false, url: 'https://github.com/sathish1433/python_app_cicd.git'
     }
     else if( config.stageName == 'pyenv'){
-        sh """/usr/bin/python -m venv myenv & source myenv/bin/activate"""
+        sh """/usr/bin/python -m venv myenv && source myenv/bin/activate"""
     }
     else if ( config.stageName == 'package'){
         sh """/usr/bin/python -r requirements.txt"""

de7676abe43010feb5a3dd92022cc44edf184f7a
Sathish <76042316+sathish1433@users.noreply.github.com>
2026-02-22 17:12:47 +0530

    Create pythonapp.groovy

---
+++ vars/pythonapp.groovy
@@ -0,0 +1,14 @@
+def call(Map config=[:]){
+    if(config.stageName == 'Checkout'){
+        git changelog: false, poll: false, url: 'https://github.com/sathish1433/python_app_cicd.git'
+    }
+    else if( config.stageName == 'pyenv'){
+        sh """/usr/bin/python -m venv myenv & source myenv/bin/activate"""
+    }
+    else if ( config.stageName == 'package'){
+        sh """/usr/bin/python -r requirements.txt"""
+    }
+    else if (config.stageName == 'Deploy'){
+        sh """/user/bin/python app.py"""
+    }
+}
